6097bf28314f0dac4a4d318ea332981bbb82cc0b
zhangkai <837578119@qq.com>
2020-11-28 14:30:46 +0800

    first

---
+++ app/src/main/java/com/bba/datacollection/MainActivity.java
@@ -0,0 +1,76 @@
+package com.bba.datacollection;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity{
+//    private TextView accelerometerView;
+//    private TextView orientationView;
+    private TextView accelerometerView_x;
+    private TextView accelerometerView_y;
+    private TextView accelerometerView_z;
+    private SensorManager sensorManager;
+    private MySensorEventListener sensorEventListener;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorEventListener = new MySensorEventListener();
+        accelerometerView_x = (TextView) this.findViewById(R.id.tvx);
+        accelerometerView_y = (TextView) this.findViewById(R.id.tvy);
+        accelerometerView_z = (TextView) this.findViewById(R.id.tvz);
+//        orientationView = (TextView) this.findViewById(R.id.orientationView);
+        //获取感应器管理器
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        //获取加速度传感器
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+
+    private final class MySensorEventListener implements SensorEventListener
+    {
+        //可以得到传感器实时测量出来的变化值
+        @Override
+        public void onSensorChanged(SensorEvent event)
+        {
+            //得到加速度的值
+            if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
+            {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                accelerometerView_x.setText("Accelerometer X: " + x);
+                accelerometerView_y.setText("Accelerometer Y: " + y);
+                accelerometerView_z.setText("Accelerometer Z: " + z);
+            }
+
+        }
+        //重写变化
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy)
+        {
+        }
+    }
+
+    //暂停传感器的捕获
+    @Override
+    protected void onPause()
+    {
+        sensorManager.unregisterListener(sensorEventListener);
+        super.onPause();
+    }
+
+}
