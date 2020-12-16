87e64bb0205a29183bf233090a0b96c29997cf4f
zhangkai <837578119@qq.com>
2020-12-16 15:31:29 +0800

    add gyroscope

--- app/src/main/java/com/bba/datacollection/MainActivity.java
+++ app/src/main/java/com/bba/datacollection/MainActivity.java
@@ -16,13 +16,12 @@ public class MainActivity extends Activity{
 //    private TextView accelerometerView;
 //    private TextView orientationView;
     private TextView net_status_check;
-    private TextView accelerometerView_x;
-    private TextView accelerometerView_y;
-    private TextView accelerometerView_z;
+    private TextView accelerometerView;
+    private TextView gyroscopeView;
+    private TextView resultView;
     private SensorManager sensorManager;
     private MySensorEventListener sensorEventListener;
     private NetworkStateReceiver netWorkStateReceiver;
-    private String net_state = "";
     private AndroidApplication app;
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -32,9 +31,9 @@ public class MainActivity extends Activity{
 //        ""https://www.cnblogs.com/zhujiabin/p/4227785.html
         sensorEventListener = new MySensorEventListener();
         net_status_check = (TextView) this.findViewById(R.id.net_status);
-        accelerometerView_x = (TextView) this.findViewById(R.id.tvx);
-        accelerometerView_y = (TextView) this.findViewById(R.id.tvy);
-        accelerometerView_z = (TextView) this.findViewById(R.id.tvz);
+        accelerometerView = (TextView) this.findViewById(R.id.accelerometer);
+        gyroscopeView = (TextView) this.findViewById(R.id.gyroscope);
+        resultView = (TextView) this.findViewById(R.id.result);
 //        orientationView = (TextView) this.findViewById(R.id.orientationView);
         //获取感应器管理器
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -60,7 +59,9 @@ public class MainActivity extends Activity{
     }
     protected void start(View v){
         Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
         Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
     }
     protected void stop(View v){
@@ -80,9 +81,22 @@ public class MainActivity extends Activity{
                 float x = event.values[0];
                 float y = event.values[1];
                 float z = event.values[2];
-                accelerometerView_x.setText("Accelerometer X: " + x);
-                accelerometerView_y.setText("Accelerometer Y: " + y);
-                accelerometerView_z.setText("Accelerometer Z: " + z);
+                x = Float.valueOf(String.format("%.3f",x));
+                y = Float.valueOf(String.format("%.3f",y));
+                z = Float.valueOf(String.format("%.3f",z));
+
+                accelerometerView.setText("Accelerometer: " + x + "," + y + "," + z);
+            }
+            //得到陀螺仪的值
+            if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE)
+            {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                x = Float.valueOf(String.format("%.3f",x));
+                y = Float.valueOf(String.format("%.3f",y));
+                z = Float.valueOf(String.format("%.3f",z));
+                gyroscopeView.setText("Gyroscope:    " + x + "," + y + "," + z);
             }
 
         }
